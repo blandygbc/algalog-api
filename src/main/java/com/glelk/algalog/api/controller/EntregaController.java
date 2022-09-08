b4ef8e9211eff0c097ba5985293e7a2877eb009f
Gabriel Blandy <gabriel_blandy@hotmail.com>
2022-09-08 09:41:25 -0300

    finished 3.4

--- src/main/java/com/glelk/algalog/api/controller/EntregaController.java
+++ src/main/java/com/glelk/algalog/api/controller/EntregaController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.glelk.algalog.api.domain.model.Entrega;
 import com.glelk.algalog.api.domain.repository.EntregaRepository;
 import com.glelk.algalog.api.domain.service.SolicitacaoEntregaService;
+import com.glelk.algalog.api.model.DestinatarioModel;
+import com.glelk.algalog.api.model.EntregaModel;
 
 @RestController
 @RequestMapping("/entregas")
@@ -41,9 +43,24 @@ public class EntregaController {
     }
 
     @GetMapping("/{entregaId}")
-    public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId) {
+    public ResponseEntity<EntregaModel> buscar(@PathVariable Long entregaId) {
         return entregaRepository.findById(entregaId)
-                .map(ResponseEntity::ok)
+                .map(entrega -> {
+                    EntregaModel entregaModel = new EntregaModel();
+                    entregaModel.setId(entrega.getId());
+                    entregaModel.setNomeCliente(entrega.getCliente().getNome());
+                    entregaModel.setDestinatario(new DestinatarioModel());
+                    entregaModel.getDestinatario().setNome(entrega.getDestinatario().getNome());
+                    entregaModel.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
+                    entregaModel.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
+                    entregaModel.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
+                    entregaModel.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
+                    entregaModel.setTaxa(entrega.getTaxa());
+                    entregaModel.setStatus(entrega.getStatus());
+                    entregaModel.setDataPedido(entrega.getDataPedido());
+                    entregaModel.setDataFinalizacao(entrega.getDataFinalizacao());
+                    return ResponseEntity.ok(entregaModel);
+                })
                 .orElse(ResponseEntity.notFound().build());
     }
 
